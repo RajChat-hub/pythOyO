009402ca2c7b2d811f4e54c3e621e066114174e7
RajChat-hub <chatterjeerajdeep124@gmail.com>
2024-09-18 19:22:51 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pythoyo",  
-    version="0.1.0",  
-    description="A fun and whimsical UyU programming language written in python", 
-    long_description=open('README.md').read(),  
+    name="pythOyO",  # Package name
+    version="0.1.0",  # Initial version
+    author="Rajdeep Chatterjee",  # my name
+    author_email="rc9775295@gmail.com",  # my email
+    description="A fun and whimsical UyU programming language written in python",  # Short description
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    author="Rajdeep Chatterjee",  
-    author_email="rc9775295@gmail.com",  
-    url="https://github.com/RajChat-hub/pythOyO",  
-    packages=find_packages(),  
-    entry_points={
-        "console_scripts": [
-            "pythoyo=pythoyo.pythoyo:main",  
-            "shyell=pythoyo.shyell:main",  
-        ],
-    },
-    install_requires=[],  
+    url="https://github.com/RajChat-hub/pythOyO",  # my GitHub repo
+    packages=find_packages(),
+    py_modules=['pythoyo', 'shyell', 'shÿell', 'styings_with_ayyows'],
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License", 
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  
+    python_requires='>=3.6',
 )
