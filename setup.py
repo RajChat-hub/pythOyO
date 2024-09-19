15eb319c38532b9a786e9f22f73f5cedd95fb6b9
RajChat-hub <chatterjeerajdeep124@gmail.com>
2024-09-20 00:22:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="pythOyO",  # Package name
     version="0.1.0",  # Initial version
-    author="Rajdeep Chatterjee",  # my name
-    author_email="rc9775295@gmail.com",  # my email
-    description="A fun and whimsical UyU programming language written in python",  # Short description
+    author="Rajdeep Chatterjee",  # Your name
+    author_email="rc9775295@gmail.com",  # Your email
+    description="A fun and whimsical UyU programming language written in Python",  # Short description
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/RajChat-hub/pythOyO",  # my GitHub repo
+    url="https://github.com/RajChat-hub/pythOyO",  # GitHub repo
     packages=find_packages(),
-    py_modules=['pythoyo', 'shyell', 'shÿell', 'styings_with_ayyows'],
+    py_modules=['pythoyo', 'shyell', 'shÿell', 'styings_with_ayyows', 'IndependentPythoyo'],  # Add IndependentPythoyo module
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -18,4 +18,10 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pythoyo = IndependentPythoyo:main',  # Run .pyoyo files
+            'pythoyo-shell = shÿell:start_shell',  # Assuming start_shell function starts the interactive shell
+        ],
+    },
 )
