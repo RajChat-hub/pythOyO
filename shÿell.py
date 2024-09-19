2e1c50c6147c5b97a04d820bd8ee9b4580458ff4
RajChat-hub <chatterjeerajdeep124@gmail.com>
2024-09-20 00:24:51 +0530

    added a third variant of shell file to start shell any where of system

--- shÿell.py
+++ shÿell.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 import os
 import pythoyo
+import sys  # Import sys to handle exit
 
-while True:
-    text = input("(｡･ÿ･｡)ﾉ★ > ")
-    if text.strip() == "cyear":
-        os.system('clear' if os.name == 'posix' else 'cls')
-        continue
-    if text.strip() == "":
-        continue
-    result, error = pythoyo.run("<stdin>", text)
+def start_shell():
+    while True:
+        text = input("(｡･ÿ･｡)ﾉ★ > ")
+        if text.strip() == "cyear":
+            os.system('clear' if os.name == 'posix' else 'cls')
+            continue
+        if text.strip() == "exyit()":  # Add the exyit() function to exit the shell
+            print("Exiting pythOyO shell.")
+            sys.exit()  # Exit the shell
+        if text.strip() == "":
+            continue
+        result, error = pythoyo.run("<stdin>", text)
 
-    if error:
-        print(error.as_string())
-    elif result:
-        if len(result.elements) == 1:
-            print(repr(result.elements[0]))
-        else:
-            print(repr(result))
+        if error:
+            print(error.as_string())
+        elif result:
+            if len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
+
+if __name__ == "__main__":
+    start_shell()
